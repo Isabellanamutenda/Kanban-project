bfb604d4abccfdb8e5f7a47f854bf189fc7cb478
Isabellanamutenda <namtendaisabella.com>
2021-09-17 18:43:36 +0300

    Fix the required changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import './style.css';
-// import 'bootstrap';
-// import Pictures from './api/getMovies.js';
-// import displayPictures from './api/diplayMovies.js';
-
-// const upcoming = new Pictures('upcoming');
-// upcoming.getData().then(() => {
-//   displayPictures(upcoming.data, 'upcoming');
-// });
-
 import './style.css';
 import buildShows from './modules/buildShows.js';
 import { showsCount } from './modules/counters.js';
