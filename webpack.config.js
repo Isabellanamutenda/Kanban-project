7bbf1590a37abf3fc0498bd3c9108c38489423ee
Isabellanamutenda <namtendaisabella.com>
2021-09-17 18:52:27 +0300

    Fix linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/kanban-project/",
+    publicPath: '/kanban-project/',
     clean: true,
   },
   module: {
